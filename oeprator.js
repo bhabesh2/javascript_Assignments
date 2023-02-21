5690072d56da52f3e8698aac1c15760ebf55b03f
bhabesh2 <jsarma058@gmail.com>
2023-02-21 17:00:23 +0530

    multiplication table added

--- oeprator.js
+++ oeprator.js
@@ -1,1 +1,26 @@
 
+// multiplication table of 19 with for loop
+let table = 6;
+
+for (let i = 1; i <=10; i++) {
+    let num = i;
+    let n = table;
+    let product = table*num+"\n";
+    console.log(`${n} * ${num} = ${product}`);
+}
+
+// simple multiplication table with out loop
+
+let table2 = 3;
+
+console.log(`${table2} * 1 = ${table2*1}`)
+console.log(`${table2} * 2 = ${table2*2}`)
+console.log(`${table2} * 3 = ${table2*3}`)
+console.log(`${table2} * 4 = ${table2*4}`)
+console.log(`${table2} * 5 = ${table2*5}`)
+console.log(`${table2} * 6 = ${table2*6}`)
+console.log(`${table2} * 7 = ${table2*7}`)
+console.log(`${table2} * 8 = ${table2*8}`)
+console.log(`${table2} * 9 = ${table2*9}`)
+console.log(`${table2} * 10 = ${table2*10}`)
+
