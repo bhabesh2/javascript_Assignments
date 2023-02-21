f3846b7f3d544a7183b3e27a96297d6ac2864368
bhabesh2 <jsarma058@gmail.com>
2023-02-21 15:06:50 +0530

    more examples added

--- Variables and typeof.js
+++ Variables and typeof.js
@@ -34,4 +34,27 @@ console.log(typeof num4)
 let value = true;
 console.log(typeof value) // type -- boolean.
 
+//8. undefined
+
+let myName;
+console.log(typeof myName);//type -- undefined;
+
+//9. null 
+
+let car = null;
+console.log(typeof car);//type -- object;
+
+//10. symbol
+
+let sym = Symbol("my symbol");
+console.log(typeof sym);//type -- symbol;
+
+//11. Objects
+
+let array = [1,2,"pw skills"];
+console.log(typeof array);//type -- object;
+
+let obj = {name:"Rahul",age:12, marks:320};
+console.log(typeof obj);//type -- object; 
+
 // note we can check data type of any variable by writing typeof in javascript. 
