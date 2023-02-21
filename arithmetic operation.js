553ed81c9aac9a57b889ca11c6a7e778d1e7f31e
bhabesh2 <jsarma058@gmail.com>
2023-02-21 18:01:22 +0530

    arithmetic_operation

--- arithmetic operation.js
+++ arithmetic operation.js
@@ -1,1 +1,39 @@
 
+// addition (+)
+
+let a = 4;
+let b = 5;
+
+console.log(`sum of a and b is ${a+b}`);
+
+// subtraction (-) 
+
+console.log(`subtraction of b and a is ${b-a}`);
+
+// multiplication (*)
+
+console.log(`multiplication of a and b ${a*b}`);
+
+// divide (/)
+
+console.log(`divide a and b ${b/a}`);
+
+// remainder(% -- modulo)
+
+console.log(`remainder of a and b ${b%a}`);
+
+// Exponentiation (**)
+
+console.log(`exponentiation a and b ${a**b}`)
+
+// sum of a and b is 9
+
+// subtraction of b and a is 1
+
+// multiplication of a and b 20
+
+// divide a and b 1.25
+
+// remainder of a and b 1
+
+// exponentiation a and b 1024
