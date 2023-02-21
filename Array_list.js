d32729e9be7312285bba5fb5b5956dce38c204e2
bhabesh2 <jsarma058@gmail.com>
2023-02-21 12:44:22 +0530

    list of 10 products

--- Array_list.js
+++ Array_list.js
@@ -1,1 +1,21 @@
 
+
+// array list of 10 products.
+
+let product_list = 
+[
+    'iphone',
+    'Dell laptop',
+    'oppo',
+    'vivo',
+    'watch',
+    't-shirt',
+    'shirt',
+    'blanket',
+    'mouse',
+    'keyboard',
+    'lens',
+];
+
+console.log(product_list)
+
