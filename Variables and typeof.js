220733bfa0fb0757f6d9e13af8bf5a1b6f816a8f
bhabesh2 <jsarma058@gmail.com>
2023-02-21 14:14:02 +0530

    variables and its types

--- Variables and typeof.js
+++ Variables and typeof.js
@@ -1,1 +1,37 @@
 
+//1. string 
+
+let name = "my name";
+console.log(typeof name);// type -- string;
+
+//2. integer
+
+let num = 12;
+console.log(typeof num);//type -- number;
+
+//3. floating value
+
+let num1 = 12.3;
+console.log(typeof num1)// type -- number;
+
+//4. infinity
+
+let num2 = Number.POSITIVE_INFINITY;
+console.log(typeof num2) // type -- number;
+
+//5. Not a number(NaN);
+
+let num3 = NaN;
+console.log(num3) //type -- NaN;
+
+//6. BigInt
+
+let num4 = 1024n; // type -- bigint;
+console.log(typeof num4)
+
+//7. boolean (true||false);
+
+let value = true;
+console.log(typeof value) // type -- boolean.
+
+// note we can check data type of any variable by writing typeof in javascript. 
