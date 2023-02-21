c369c0946de02be24a44371f39e1456a41659fcb
bhabesh2 <jsarma058@gmail.com>
2023-02-21 15:25:30 +0530

    some example of valid or invalid variables

--- valid or invalid variables.js
+++ valid or invalid variables.js
@@ -1,1 +1,19 @@
 
+//valid variable
+
+let student_name = "Rahul";
+let age = 12;
+console.log(student_name)// OUT PUT = Rahul;
+console.log(age) // OUT PUT = 12;
+
+// invalid variable
+
+let @name = "Rahul";
+let 1age = 12;
+
+console.log(@name); // syntaxError: invalid or unexpected token;
+console.log(1age); /// syntaxError: invalid or unexpected token;
+
+//note: we can't start variables with numbers or symbols it will be invalid. but there is a exception about $ and (_) symbol. but we can use numbers after a variable like below; but we still can't use symbols after a variable. 
+
+let age1 = 12;
