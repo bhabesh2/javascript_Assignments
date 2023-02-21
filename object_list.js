2b53e858e74a6810acf244fd3acd04bcbbcc4247
bhabesh2 <jsarma058@gmail.com>
2023-02-21 13:13:35 +0530

    key_value pair list

--- object_list.js
+++ object_list.js
@@ -1,1 +1,21 @@
 
+
+let obj = {
+    1:"Bhabesh",
+    2:"Rahul",
+    3:"Alka",
+    4:"Mithun",
+    5:"Kuldeep",
+    6:"Shivam",
+};
+
+console.log(obj[1])
+console.log(obj[2])
+console.log(obj[3])
+console.log(obj[4]);
+
+//this is how we can access the data inside an object.
+
+// there are two notations one is bracket notation and next is dot notation.
+// example console.log(obj[1]) bracket notation.
+// example console.log(obj.name) dot notation.
