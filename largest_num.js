aaa7d357c17e8c85d614e77066819911114125ee
bhabesh2 <jsarma058@gmail.com>
2023-02-21 21:17:59 +0530

    largest and smallest num

--- largest_num.js
+++ largest_num.js
@@ -1,1 +1,19 @@
 
+let num = 12;
+let num1 = 13;
+
+if(num > num1){
+    console.log(`num is the largest`);// if the condition true then it will execute;
+}
+else{
+    console.log(`num1 is the largest`);// true;
+}
+
+// example--2
+
+if(num < num1){
+    console.log(`num is the smallest`);// true;
+}
+else{
+    console.log(`num1 is the smallest`);
+}
