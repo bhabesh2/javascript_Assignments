bda1638d2d3fadcae16e316b5a3b267d76e50545
bhabesh2 <jsarma058@gmail.com>
2023-02-21 22:23:11 +0530

    check this example

--- Ternary Conditions.js
+++ Ternary Conditions.js
@@ -1,1 +1,19 @@
 
+let age = 12;
+
+if(age>=18){
+    console.log(`apply for license`);
+}
+else{
+    console.log(`Not applicable`);
+}
+
+// ternary conditions
+
+(age>=18)?console.log(`apply for license`):console.log(`not eligible`);
+
+// 2nd method 
+
+console.log(age>=18 ? "apply for license":"can't apply");
+
+// here first console defined as if condition and the 2nd condition is defined as else condition.
